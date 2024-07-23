46656f769bed99a18df5b980cbe9b03569009cbf
sambaliga06 <118614578+sambaliga06@users.noreply.github.com>
2024-07-23 20:04:10 +0530

    Website that generate random memes

---
+++ meme/meme.js
@@ -0,0 +1,22 @@
+const generateMemeBtn = document.querySelector(".meme-gen .gen-meme-btn");
+const memeImage = document.querySelector(".meme-gen img");
+const memeTitle = document.querySelector(".meme-gen .meme-title");
+const memeAuthor = document.querySelector(".meme-gen .meme-author");
+
+const updateDetails = (url, title, author) => {
+    memeImage.setAttribute("src", url);
+    memeTitle.innerHTML = title;
+    memeAuthor.innerHTML = `Meme by: ${author}`;
+};
+
+const generateMeme = () => {
+    fetch("https://meme-api.com/gimme")
+        .then((response) => response.json())
+        .then((data) => {
+            updateDetails(data.url, data.title, data.author);
+        });
+};
+
+generateMemeBtn.addEventListener("click", generateMeme);
+
+generateMeme(); // This will fetch and display a meme when the page loads
